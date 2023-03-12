f6cde1cb7cfb0d721fe11ad7e854b5ac1aeb672f
Shehryar Hassan <phonetech195@gmail.com>
2023-03-12 17:15:10 +0100

    adding effects form

--- src/todo/taskForm/taskForm.tsx
+++ src/todo/taskForm/taskForm.tsx
@@ -5,16 +5,30 @@ import {
   Stack,
   TextField,
 } from "@fluentui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useInput from "./useInputs";
 
 const TaskForm = () => {
+  const [showMessage, setShowMessage] = useState<{
+    type: MessageBarType;
+    message: string;
+  }>({ type: MessageBarType.success, message: "" });
   const title = useInput("");
   const description = useInput("");
 
+  useEffect(() => {
+    if (showMessage.message) {
+      setTimeout(() => {
+    setShowMessage({type: MessageBarType.success, message: ""})
+  }, 1000)
+}
+  }, [showMessage.message])
+
   const onFormSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-  } 
+
+    setShowMessage({ type: MessageBarType.success, message: "Task Added" });
+  };
   return (
     <form onSubmit={onFormSubmit}>
       <TextField label="Title" required {...title} />
@@ -22,9 +36,11 @@ const TaskForm = () => {
 
       <Stack horizontal tokens={{ childrenGap: 20 }} style={{ marginTop: 20 }}>
         <Stack style={{ width: "80%" }}>
-          <MessageBar messageBarType={MessageBarType.success}>
-            Task Added
-          </MessageBar>
+          {showMessage.message && (
+            <MessageBar messageBarType={MessageBarType.success}>
+              Task Added
+            </MessageBar>
+          )}
         </Stack>
 
         <Stack style={{ width: "20%" }}>
