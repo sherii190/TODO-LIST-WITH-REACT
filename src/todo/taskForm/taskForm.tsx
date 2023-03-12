60bb10f726c9cece616be424eb5e2ad63b666903
Shehryar Hassan <phonetech195@gmail.com>
2023-03-12 16:33:07 +0100

    task form button feild styling

--- src/todo/taskForm/taskForm.tsx
+++ src/todo/taskForm/taskForm.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import {
+  MessageBar,
+  MessageBarType,
+  PrimaryButton,
+  Stack,
+  TextField,
+} from "@fluentui/react";
+import React from "react";
 
 const taskForm = () => {
   return (
-    <div>
-      This is task  form </div>
+    <Stack>
+      <TextField label="Title" required />
+      <TextField label="Description" multiline rows={4} />
+
+      <Stack horizontal tokens={{ childrenGap: 20}} style={{marginTop: 20}}>
+        <Stack style={{ width: "80%"}}>
+          <MessageBar messageBarType={MessageBarType.success}>
+            Task Added
+          </MessageBar>
+        </Stack>
+
+        <Stack style={{ width: "20%" }}>
+          <PrimaryButton text="Add Task" />
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
 
